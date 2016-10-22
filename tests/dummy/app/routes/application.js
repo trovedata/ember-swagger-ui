bf3bac449d6a98819519a84a28b4277a2a8bb306
Ryan Connolly <rynam0@gmail.com>
2016-10-21 20:57:29 -0400

    Publishing to github pages

--- tests/dummy/app/routes/application.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-
-  setupController(controller) {
-    controller.set('authz', { name: 'api_key', value: 'my-api-key', type: 'query'} );
-  }
-
-});
