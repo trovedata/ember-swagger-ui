519fda93909b2203ee360fca1bce367606dfe40f
Ricardo Beas <rbeas@trovedata.com>
2017-11-09 13:02:58 -0500

    Moving all dependencies to node

--- blueprints/ember-swagger-ui/index.js
+++ blueprints/ember-swagger-ui/index.js
@@ -2,13 +2,5 @@
 'use strict';
 
 module.exports = {
-  normalizeEntityName: function() {},
-
-  beforeInstall: function(options) {
-    return this.addBowerPackagesToProject([
-      // we want 3.3.1
-      { name: 'swagger-ui', target: '~2.2.10' },
-      { name: 'jquery-migrate', target: '1.2.1' }
-    ]);
-  }
+  normalizeEntityName: function() {}
 };
